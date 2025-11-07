04325562fa559c5128230e8fab69073ffcbb9201
fer <20233005420@estudantes.ifpr.edu.br>
2025-11-07 11:42:59 -0300

    implementações (07/11)

--- cars-main/src/main/java/br/edu/ifpr/cars/domain/Driver.java
+++ cars-main/src/main/java/br/edu/ifpr/cars/domain/Driver.java
@@ -2,6 +2,8 @@ package br.edu.ifpr.cars.domain;
 
 import java.time.LocalDate;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -16,17 +18,17 @@ import lombok.Data;
 public class Driver {
 
     @Id
-    @GeneratedValue 
+    @GeneratedValue
     Long id;
-    
+
     @NotBlank(message = "Digite seu nome...")
-    @Size(min=2, max=50)
+    @Size(min = 2, max = 50)
     String name;
-    
+
     LocalDate birthDate;
 
     @NotBlank(message = "Digite seu cpf...")
-    // @Pattern(regexp=, flags=0)
+    @CPF
     String cpf;
 
     @NotBlank(message = "Digite seu email...")
@@ -41,6 +43,5 @@ public class Driver {
 // email --> campo obrigatório, não pode aceitar espaços em branco e deve estar no padrão de email
 // cpf --> campo obrigatório, não pode aceitar espaços em branco e deve estar no padrão de cpf
 //fazer rodar
-
 //beans - verificações
 //controller - validar
